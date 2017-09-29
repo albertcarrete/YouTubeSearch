1d0748ea47d82311c3f70b51d0c6ec9a074171cf
Albert Carrete <acarrete@Alberts-MacBook-Pro.local>
2017-09-29 12:37:21 -0700

    added lodash for debounce to prevent instant reloading on search

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -21,10 +21,17 @@ class SearchBar extends Component{
             <div className="search-bar">
                  <input
                     value={this.state.term}
-                    onChange={event=> this.setState({ term: event.target.value })} 
+                    onChange={event=> this.onInputChange(event.target.value)} 
                     />
             </div>
         );
+    }
+    // custom function
+    onInputChange(term){
+        // set the state with the updated term
+        this.setState({ term: event.target.value })
+        // Callback function passed down from index.js
+        this.props.onSearchTermChange(term)
     }
 }
 
